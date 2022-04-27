17bc34c498a710fc05fa573063e2e9975bcfaea5
XianLin <wanganning5201314@163.com>
2022-04-27 17:34:42 +0800

    添加拦截器

--- src/main/java/xyz/xianlin/config/SpringMvcSupport.java
+++ src/main/java/xyz/xianlin/config/SpringMvcSupport.java
@@ -1,11 +1,17 @@
 package xyz.xianlin.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import xyz.xianlin.controller.interceptor.WebInterceptor;
 
 @Configuration // 设置为配置类, 用于设置非MVC资源映射
 public class SpringMvcSupport extends WebMvcConfigurationSupport {
+    @Autowired
+    private WebInterceptor webInterceptor;
+    
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/pages/**").addResourceLocations("/pages/"); // 设置静态资源映射
@@ -13,5 +19,11 @@ public class SpringMvcSupport extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/js/**").addResourceLocations("/js/");
         registry.addResourceHandler("/img/**").addResourceLocations("/img/");
         registry.addResourceHandler("/element-ui/**").addResourceLocations("/element-ui/");
+    }
+    
+    
+    @Override // 注册拦截器
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(webInterceptor).addPathPatterns("/users"); // 拦截请求
     }
 }
